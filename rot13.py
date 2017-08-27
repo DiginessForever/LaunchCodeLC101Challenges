85d5c4aed676470f27f40153e43c7ced4a43b659
DiginessForever <diginess@gmail.com>
2017-08-27 13:25:53 -0500

    corrected the lack of the W character in the upper case alphabet

--- rot13.py
+++ rot13.py
@@ -1,5 +1,5 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-upperAlphabet = ['A','B','C','D','E','F','G','H', 'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+upperAlphabet = ['A','B','C','D','E','F','G','H', 'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 def rot13(message):
 	encryptedMessage = ''
